9f572a97372c773400c3dfd246ab086fdd63462e
eogieglo <eric.ogieglo@gmail.com>
2022-04-30 16:56:42 -0300

    primera-prueba-unitaria

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -4,4 +4,11 @@ class TodoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "title should not be empty" do
+    todo = Todo.new
+    todo.save
+    #assert_not todo.persisted?
+    assert todo.errors.any?, "No encontramos errores"
+  end
 end
