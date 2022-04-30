fa35206898d55acfc3403a7c7309fa2c4778f1a4
eogieglo <eric.ogieglo@gmail.com>
2022-04-30 16:19:08 -0300

    generadores-de-codigo ADD Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+  get 'example/demo'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/hello", to: "main#hello"
 
