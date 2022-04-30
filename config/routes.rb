2fa0a661f24c900c1c6c63977ae360934849ce40
eogieglo <eric.ogieglo@gmail.com>
2022-04-30 14:53:20 -0300

    introduccion-a-las-rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/hello", to: "main#hello"
+
+  # Por convencion la primera parte del path es el controlador
+  # y la segunda el metodo
+  get "/cursos/gratuitos"
+  # cursos_controller 
+  # En el archivo veriamos CursosController def gratuitos end
+
+  get "cursos", to: "courses#index"
+  # Buscara Rails lo mismo
 end
