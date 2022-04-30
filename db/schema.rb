9b87c6f68390b1c0ecaa7a26b285c31df7272ec9
eogieglo <eric.ogieglo@gmail.com>
2022-04-30 20:45:31 -0300

    crear-la-tabla-y-el-modelo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_30_205418) do
+ActiveRecord::Schema.define(version: 2022_04_30_234126) do
+
+  create_table "photos", force: :cascade do |t|
+    t.string "title"
+    t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
