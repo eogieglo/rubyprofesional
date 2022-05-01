0b07a0e490357112707030593b93f7f9e53fad11
eogieglo <eric.ogieglo@gmail.com>
2022-04-30 21:04:21 -0300

    pruebas-unitarias-y-validaciones

--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -14,4 +14,24 @@ class PhotoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should have a title value" do
+    photo = Photo.new(image_url:"algo.jpg")
+    assert_not photo.save
+  end
+
+  test "should have an image_url value" do
+    photo = Photo.new(title:"algo.jpg")
+    assert_not photo.save
+  end
+
+  test "should not save non valid image urls" do
+    photo = Photo.new(title:"algo", image_url:"algo")
+    assert_not photo.save
+  end
+
+  test "should save valid image urls" do
+    photo = Photo.new(image_url:"algo.jpg", title: "Algo")
+    assert photo.save
+  end
+  
 end
