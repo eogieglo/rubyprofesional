06dda7bef78722f6ebcac0aa6105dc6b725b62a9
eogieglo <eric.ogieglo@gmail.com>
2022-04-30 21:11:31 -0300

    rutas-rest

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   
   resources :tweets
-
-  get "/photos/index"
+  resources :photos
+  
+  
+  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/hello", to: "main#hello"
-
   # Usaremos el metodo root para definir el path de inicio
   root to: "main#hello"
-
   get "saludo", to: "main#hi"
   post "hi", to: "main#greetings" 
 end
