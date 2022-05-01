05f3457831a73abd70da75d2ef7bb8250bd60663
eogieglo <eric.ogieglo@gmail.com>
2022-04-30 21:09:08 -0300

    crear-el-controlador Photos

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   resources :tweets
 
+  get "/photos/index"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/hello", to: "main#hello"
 
